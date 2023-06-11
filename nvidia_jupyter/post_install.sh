83bff88d64afbedb9da9f4accf8d99ff029cc8e4
Jianlin Shi <jianlins@users.noreply.github.com>
2023-06-11 17:18:46 -0600

    add nvidia_jupyter

---
+++ nvidia_jupyter/post_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# install python packages or models that not included when doing pip install or conda/mamaba install
+python3 -c "import nltk; nltk.download('punkt');"
+python3 -m spacy download en_core_web_sm
+
