bac199cb7d2cf1445ba8d10aafcf71d352d5c3ac
Jianlin Shi <jianlins@users.noreply.github.com>
2023-06-12 04:34:34 -0600

    check /tmp

--- costmos/post_install.sh
+++ costmos/post_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-pip install git+https://github.com/NVIDIA/TransformerEngine.git@stable
+# need devel image, which results over size error atm, 
+# pip install git+https://github.com/NVIDIA/TransformerEngine.git@stable
 # install python packages or models that not included when doing pip install or conda/mamaba install
 python3 -c "import nltk; nltk.download('punkt');"
 python3 -m spacy download en_core_web_sm
