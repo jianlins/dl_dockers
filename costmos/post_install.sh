e8fde5e53f09a2dab527390f80f23f57ad8b3460
Jianlin Shi <jianlins@users.noreply.github.com>
2023-06-12 01:34:51 -0600

    fix apt install

--- costmos/post_install.sh
+++ costmos/post_install.sh
@@ -4,5 +4,6 @@ NVTE_FRAMEWORK=pytorch pip install git+https://github.com/NVIDIA/TransformerEngi
 # install python packages or models that not included when doing pip install or conda/mamaba install
 python3 -c "import nltk; nltk.download('punkt');"
 python3 -m spacy download en_core_web_sm
-python3 -c "import jdk;jdk.install('17', vendor='Corretto');"
+# the jdk version is passed through bash script argument (in Dockerfile set openjdk_version)
+python3 -c "import jdk;jdk.install('$1', vendor='Corretto');"
 
