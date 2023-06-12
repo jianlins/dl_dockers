5de08a5e8a8426a543d202dece270f8bbfef164f
Jianlin Shi <jianlins@users.noreply.github.com>
2023-06-11 20:03:45 -0600

    test costmos

--- costmos/post_install.sh
+++ costmos/post_install.sh
@@ -2,4 +2,5 @@
 # install python packages or models that not included when doing pip install or conda/mamaba install
 python3 -c "import nltk; nltk.download('punkt');"
 python3 -m spacy download en_core_web_sm
+python3 -c "import jdk;jdk.install('17', vendor='Corretto');"
 
