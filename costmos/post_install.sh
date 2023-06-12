7c9b51a90e5d3190725d9c11a4c5926c875042f2
Jianlin Shi <jianlins@users.noreply.github.com>
2023-06-12 01:54:35 -0600

    refactor git install TransformerEngine

--- costmos/post_install.sh
+++ costmos/post_install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# https://docs.nvidia.com/deeplearning/transformer-engine/user-guide/installation.html
-NVTE_FRAMEWORK=pytorch pip install git+https://github.com/NVIDIA/TransformerEngine.git@stable
 # install python packages or models that not included when doing pip install or conda/mamaba install
 python3 -c "import nltk; nltk.download('punkt');"
 python3 -m spacy download en_core_web_sm
