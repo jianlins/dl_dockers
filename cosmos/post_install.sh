b85842ed05c07602972979822686cfaa690d16e2
Jianlin Shi <jianlins@users.noreply.github.com>
2023-06-16 12:37:26 -0600

    direct install corretto jdk

--- cosmos/post_install.sh
+++ cosmos/post_install.sh
@@ -4,6 +4,4 @@
 # install python packages or models that not included when doing pip install or conda/mamaba install
 python3 -c "import nltk; nltk.download('punkt');"
 python3 -m spacy download en_core_web_sm
-# the jdk version is passed through bash script argument (in Dockerfile set openjdk_version)
-python3 -c "import jdk;jdk.install('$1', vendor='Corretto');"
 
