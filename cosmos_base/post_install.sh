4da6ef99d9d90e162b8f314ac6496cc4575162fc
Jianlin <jianlinshi.cn@gmail.com>
2023-11-22 22:35:59 +0000

    update cosmos_base with pyspark 3.5.0 and gensim

--- cosmos_base/post_install.sh
+++ cosmos_base/post_install.sh
@@ -4,4 +4,4 @@
 # install python packages or models that not included when doing pip install or conda/mamaba install
 python3 -c "import nltk; nltk.download('punkt');"
 python3 -m spacy download en_core_web_sm
-
+python3 -c "import sparknlp; spark=sparknlp.start(); spark.stop();"
