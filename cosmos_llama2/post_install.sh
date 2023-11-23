eb4c6634ef22c530eaa4d19df57c6212398bdfe9
Jianlin <jianlinshi.cn@gmail.com>
2023-11-23 19:09:53 +0000

    update spark to 3.5.0

--- cosmos_llama2/post_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# need devel image, which results over size error atm, 
-# pip install git+https://github.com/NVIDIA/TransformerEngine.git@stable
-# install python packages or models that not included when doing pip install or conda/mamaba install
-python3 -c "import nltk; nltk.download('punkt');"
-python3 -m spacy download en_core_web_sm
-
