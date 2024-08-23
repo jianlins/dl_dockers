dde17e21147254d41f684f2ba821d2050936bf40
Jianlin Shi <jianlins@users.noreply.github.com>
2024-08-23 14:15:53 -0600

    test

--- win_TMAC/post_build.sh
+++ win_TMAC/post_build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 # This is a simple bash script that prints a message and the current date
-conda activate tvm-build
 git clone --recursive https://github.com/microsoft/T-MAC.git
 cd T-MAC
-cd 3rdparty/tvm
+cd 3rdparty\tvm
 mkdir build
-cp cmake/config.cmake build
+cp cmake\config.cmake build
 cd build
 cmake .. -A x64
 cmake --build . --config Release -- /m
-cd ../../../  # back to project root directory
+cd ..\..\..\  # back to project root directory
+pwd
+ls
 $env:MANUAL_BUILD = "1"
-$env:PYTHONPATH = "$pwd/3rdparty/tvm/python"
+$env:PYTHONPATH = "$pwd\3rdparty\tvm\python"
+pip install . -v  # or pip install -e . -v
+
 pip install . -v  # or pip install -e . -v
