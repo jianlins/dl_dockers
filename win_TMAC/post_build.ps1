4badd08eb30a73a1ed25680aa6ad2736a80c775b
Jianlin Shi <jianlins@users.noreply.github.com>
2024-09-04 04:24:20 -0600

    add requirements.txt install

--- win_TMAC/post_build.ps1
+++ win_TMAC/post_build.ps1
@@ -1,6 +1,7 @@
 # This is a simple bash script that prints a message and the current date
 git clone --recursive https://github.com/microsoft/T-MAC.git
 cd T-MAC
+pip install --no-input -r requirements.txt
 cd 3rdparty\tvm
 mkdir build
 cp cmake\config.cmake build
