c1650f67744937e6e782100b724ae5c6bc975bee
Jianlin Shi <jianlins@users.noreply.github.com>
2024-11-27 15:38:37 -0700

    testtest

--- win_llamacppgpu/post_build.ps1
+++ win_llamacppgpu/post_build.ps1
@@ -2,17 +2,22 @@ param($location)
 # This is a simple bash script that prints a message and the current date
 
 echo 'done'
-echo "conda activate location:" $location
+Write-Output "conda activate location: $location"
 conda activate $location
 nvcc --version
 
-[System.Environment]::SetEnvironmentVariable('CUDA_HOME', '$location\Library', [System.EnvironmentVariableTarget]::User)
+$cudaHome = "$envPath\Library"
+[System.Environment]::SetEnvironmentVariable('CUDA_HOME', $cudaHome, [System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable('CUDAToolkit_ROOT', $cudaHome, [System.EnvironmentVariableTarget]::User)
+
+
 $currentPath = [System.Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::User)
 $newPath = "$currentPath;$location\Library\bin"
 [System.Environment]::SetEnvironmentVariable('Path', $newPath, [System.EnvironmentVariableTarget]::User)
 
-Write-Output "CUDA_HOME has been set to: $env:CUDA_HOME"
-Write-Output "Updated Path: $env:Path"
+Write-Output "CUDA_HOME has been set to: $cudaHome"
+Write-Output "CUDAToolkit_ROOT has been set to: $cudaHome"
+Write-Output "Updated PATH: $newPath"
 
 $env:CMAKE_ARGS="-DGGML_CUDA=on"
 pip install llama-cpp-python[server]
