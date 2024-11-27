ed18a15c931d1f98bca0009fb29dd338fc90e9fb
Jianlin Shi <jianlins@users.noreply.github.com>
2024-11-27 13:13:55 -0700

    add cuda toolkit path manually

--- win_llamacppgpu/post_build.ps1
+++ win_llamacppgpu/post_build.ps1
@@ -1,7 +1,13 @@
 # This is a simple bash script that prints a message and the current date
 echo 'done'
-echo "conda activate $args[0]"
-conda activate $args[0]
+[System.Environment]::SetEnvironmentVariable('CUDA_HOME', '$args\Library', [System.EnvironmentVariableTarget]::User)
+$currentPath = [System.Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::User)
+$newPath = "$currentPath;$args\Library\bin"
+[System.Environment]::SetEnvironmentVariable('Path', $newPath, [System.EnvironmentVariableTarget]::User)
+
+Write-Output "CUDA_HOME has been set to: $env:CUDA_HOME"
+Write-Output "Updated Path: $env:Path"
+
 $env:CMAKE_ARGS="-DGGML_CUDA=on"
 pip install llama-cpp-python[server]
 # git clone --recursive https://github.com/microsoft/T-MAC.git
