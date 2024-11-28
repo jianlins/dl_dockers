4f40408e6d16619d265847b28b40c6d67ad208de
Jianlin Shi <jianlins@users.noreply.github.com>
2024-11-28 11:00:11 -0700

    test

--- win_llamacppgpu/pre_build.ps1
+++ win_llamacppgpu/pre_build.ps1
@@ -5,4 +5,5 @@ param($location)
 Invoke-WebRequest -Uri https://github.com/jianlins/llama-cpp-python/releases/download/main-cu118/llama_cpp_python-0.3.2-cp310-cp310-win_amd64.whl  -OutFile ".\llama_cpp_python.whl"
 pwd
 ls
+pip install "llama_cpp_python.whl" --verbose
 pip install "llama_cpp_python.whl[server]" --verbose
