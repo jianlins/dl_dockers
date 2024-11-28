7cf894ac56708acf2877af8f07ed22164a637897
Jianlin Shi <jianlins@users.noreply.github.com>
2024-11-28 11:02:39 -0700

    test

--- win_llamacppgpu/pre_build.ps1
+++ win_llamacppgpu/pre_build.ps1
@@ -2,8 +2,8 @@ param($location)
 # This is a simple bash script that prints a message and the current date
 
 # use my precompiled (with cuda enabled) wheel file
-Invoke-WebRequest -Uri https://github.com/jianlins/llama-cpp-python/releases/download/main-cu118/llama_cpp_python-0.3.2-cp310-cp310-win_amd64.whl  -OutFile ".\llama_cpp_python.whl"
+Invoke-WebRequest -Uri https://github.com/jianlins/llama-cpp-python/releases/download/main-cu118/llama_cpp_python-0.3.2-cp310-cp310-win_amd64.whl  -OutFile ".\llama_cpp_python-0.3.2-cp310-cp310-win_amd64.whl"
 pwd
 ls
-pip install "llama_cpp_python.whl" --verbose
-pip install -I "llama_cpp_python.whl[server]" --verbose
+pip install "llama_cpp_python-0.3.2-cp310-cp310-win_amd64.whl" --verbose
+pip install -I "llama_cpp_python-0.3.2-cp310-cp310-win_amd64.whl[server]" --verbose
