12141fa0cb4b8ee0f300999c10e3a6765d1f4b39
Jianlin Shi <jianlins@users.noreply.github.com>
2024-11-28 10:57:51 -0700

    test

--- win_llamacppgpu/pre_build.ps1
+++ win_llamacppgpu/pre_build.ps1
@@ -4,5 +4,4 @@ param($location)
 # use my precompiled (with cuda enabled) wheel file
 Invoke-WebRequest -Uri https://github.com/jianlins/llama-cpp-python/releases/download/main-cu118/llama_cpp_python-0.3.2-cp310-cp310-win_amd64.whl  -OutFile ".\llama_cpp_python.whl"
 pwd
-ls -l
-pip install ".\llama_cpp_python.whl[server]" --verbose
+ls
