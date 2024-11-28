6e88fe2eefb1443a2a283fa057ffbffbaa1333ac
Jianlin Shi <jianlins@users.noreply.github.com>
2024-11-27 19:04:22 -0700

    test

--- win_llamacppgpu/post_build.ps1
+++ win_llamacppgpu/post_build.ps1
@@ -18,14 +18,13 @@ $cudaInclude ="$cudaHome\include"
 [System.Environment]::SetEnvironmentVariable('CUDAToolkit_ROOT', $cudaHome, [System.EnvironmentVariableTarget]::User)
 [System.Environment]::SetEnvironmentVariable('CUDAToolkit_INCLUDE_DIRECTORIES', $cudaInclude, [System.EnvironmentVariableTarget]::User)
 
-
-
 $currentPath = [System.Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::User)
 $newPath = "$currentPath;$location\Library\bin"
 [System.Environment]::SetEnvironmentVariable('Path', $newPath, [System.EnvironmentVariableTarget]::User)
 
 Write-Output "CUDA_HOME has been set to: $cudaHome"
 Write-Output "CUDAToolkit_ROOT has been set to: $cudaHome"
+Write-Output "CUDAToolkit_INCLUDE_DIRECTORIES has been set to: $cudaInclude"
 Write-Output "Updated PATH: $newPath"
 
 $env:CMAKE_ARGS="-DGGML_CUDA=on"
